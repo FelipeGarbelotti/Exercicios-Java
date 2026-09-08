45b6989a6d3f33e1f3a0a990d1ca992c1819f1e3
FelipeGarbelotti <felipe.mgarbelotti@gmail.com>
2026-09-08 12:58:07 -0300

    Primeiro exercicio em Java

---
+++ conversoes/src/exercicios/Exercicios.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+public class Exercicios {
+    public static void main(String[] args){
+        String valorstring = "10";
+        int valorint = Integer.parseInt(valorstring);
+
+        System.out.println(valorint+10);
+
+        double valorDouble = 3.54;
+        int valorInt = (int) valorDouble;
+
+        System.out.println(valorInt);
+
+        String idadeString = "23";
+        int idadeInt = Integer.parseInt(idadeString);
+        System.out.println(idadeInt+10);
+
+    }
+}
